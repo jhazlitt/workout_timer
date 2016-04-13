2afd91ebb83cdeb0543e4c8c2b19aee298f5a7ea
jhazlitt <bagelday@gmail.com>
2016-04-12 20:02:26 -0600

    Fixed issue where tada.mp3 was not playing.  Included click_timer_score.txt

--- click_timer.py
+++ click_timer.py
@@ -2,21 +2,18 @@ import math
 import os
 import time
 
-count = 0
+count = 0 
 with open('click_timer_score.txt') as f:
 	lines = f.readlines()
 
-#print 'Score to beat: ' + str(lines[0])
-
 while True:
-	#os.system('clear')
 	highScore = lines[0]
 	print 'Score to beat: ' + str(highScore)
 	print str(count) + ' done. 1 minute timer (enter to begin, q to quit)'
 	userInput = raw_input()
 	if (userInput == ''):
 		count = count + 1
-		if (count > highScore) or (count == 60):
+		if (int(count) > int(highScore)):
 			os.system('mpg123 tada.mp3')
 		os.system('mpg123 party_horn.mp3')
 		startTime = time.time()
