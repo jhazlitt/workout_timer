c6a53892939165ead2955fc6d674ea61a8e1e04e
John Hazlitt <bagelday@gmail.com>
2017-04-16 23:21:37 -0600

    Added a script for the Reddit Recommended Bodyweight Routine

---
+++ recommended_routine.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+secondWait = 90
+
+lines = open('workout_list.txt')
+
+for line in lines:
+	print line
+	userInput = raw_input('Continue?')
+	os.system('mpg123 party_horn.mp3')
+	os.system('clear')
+	print(str(secondWait) + ' second break.')
+	time.sleep(90)	
+	os.system('aplay doorbell.wav')
+	os.system('clear')
+
+os.system('mpg123 tada.mp3')
+os.system('clear')
+print('Workout complete!')
